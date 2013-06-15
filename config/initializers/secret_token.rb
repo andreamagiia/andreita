8bede4f323773c2693127d0d9f88e6a35009b8b5
Andrea Pastor Guevara <andrea@magiia.com>
2013-06-15 01:05:45 -0500

    Mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andreita::Application.config.secret_token = 'c0c864fc2971ad46059c692a85a94d7093fa0fa32e73a3623c6e96b24ccbbf3a9ee39f757a8b33cbb65c1bd06b45e2f4ab36687e2c9dfef45da8a02616816def'
